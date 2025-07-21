1ce36e5a4d483229862f90b3971f68c2255c1e16
Xiu <nuannuan7@gmail.com>
2025-07-22 11:30:52 +1200

    logo

--- frontend/src/routes/signup.tsx
+++ frontend/src/routes/signup.tsx
@@ -14,7 +14,7 @@ import { InputGroup } from "@/components/ui/input-group"
 import { PasswordInput } from "@/components/ui/password-input"
 import useAuth, { isLoggedIn } from "@/hooks/useAuth"
 import { confirmPasswordRules, emailPattern, passwordRules } from "@/utils"
-import Logo from "/assets/images/fastapi-logo.svg"
+import Logo from "/assets/images/fastapi-logo11.svg"
 
 export const Route = createFileRoute("/signup")({
   component: SignUp,
@@ -70,6 +70,7 @@ function SignUp() {
             src={Logo}
             alt="FastAPI logo"
             height="auto"
+            width = "300"
             maxW="2xs"
             alignSelf="center"
             mb={4}
